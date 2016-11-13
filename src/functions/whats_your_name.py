2486b149fbe48e8082d033773e00c069c3ddf4d5
Roniel Bernas <roniel.bernas@crmworksa.asia>
2016-11-13 17:47:10 +0800

    Start learning functions, started slowly with arguments

---
+++ src/functions/whats_your_name.py
@@ -0,0 +1,10 @@
+
+def ask_name( name ):
+	
+	"This prints a passed string into this function"
+
+	print ("you think %s is the GOAT" %(name))
+	return
+
+rapper = input("who's the best rapper eva? ")
+ask_name(rapper);
