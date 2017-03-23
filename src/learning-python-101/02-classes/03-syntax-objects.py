e6fa7d260f32d75a43c3c10c78992be2e93400d6
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-03-23 18:11:33 +0800

    eggsample

---
+++ src/learning-python-101/02-classes/03-syntax-objects.py
@@ -0,0 +1,17 @@
+class Egg:
+    def __init__(self, kind="fried"):
+        self.kind = kind
+
+    def whatKind(self):
+        return self.kind
+
+
+def main():
+    fried = Egg()
+    scrambled = Egg("scrambled")
+    print(fried.whatKind())
+    print(scrambled.whatKind())
+
+
+if __name__ == '__main__':
+    main()
