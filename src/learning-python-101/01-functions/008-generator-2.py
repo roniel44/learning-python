2cf9f0f7d07589b9b788ab77edd95c2bcd680dce
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-03-24 14:19:57 +0800

    generator 2 using yield

--- src/learning-python-101/01-functions/008-generator-2.py
+++ src/learning-python-101/01-functions/008-generator-2.py
@@ -1,26 +1,11 @@
-def inclusive_range(*args):
-    numargs = len(args)
-    if numargs < 1:
-        raise TypeError('Requires at least one argument')
-
-    elif numargs == 1:
-        stop = args[0]
-        start = 0
-        step = 1
-    elif numargs == 2:
-        (start, stop) = args
-        step = 1
-    elif numargs == 3:
-        (start,stop, step) = args
-    else:
-        raise TypeError('inclusive_range got at most 3 arguments, got {}' . format(numargs))
-
-    i = start
-    while i <= stop:
-        yield i
-        i += step
+def my_generator(n):
+    yield n
+    yield n + 1
 
 
-for i in inclusive_range(25, 13, 5):
-    print(i)
+def main():
+    for n in my_generator(6):
+        print(n)
 
+if __name__ == '__main__':
+    main()
