c43df5d28da7c93b7e3ff6609755fa29c6f885ad
Roniel Bernas <roniel@eligeo.com>
2017-11-04 22:37:22 +0800

    new for loop examples  - items in object  - in range

--- src/learning-python-101/00-basics/010-for-loop.py
+++ src/learning-python-101/00-basics/010-for-loop.py
@@ -16,9 +16,20 @@ def read_string():
         print('iteration done')
 
 
+def range_test():
+    my_list = {1, 2, 3}
+
+    for w in my_list:
+        print('item is ', w)
+
+    for p in range(12):
+        print('range 12 item number is ', p)
+
+
 def main():
     read_lines()
     read_string()
+    range_test()
 
 
 if __name__ == '__main__':
