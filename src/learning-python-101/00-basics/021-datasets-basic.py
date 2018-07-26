1a60a07b1ce45a4800d25544066d5591379617fa
Roniel Bernas <roniel@eligeo.com>
2018-07-26 16:57:52 +0800

    learned about renaming columns

--- src/learning-python-101/00-basics/021-datasets-basic.py
+++ src/learning-python-101/00-basics/021-datasets-basic.py
@@ -1,8 +1,8 @@
 import pandas as pd
+stats = pd.read_csv('files/DemographicData.csv')
 
 
-def main():
-    stats = pd.read_csv('files/DemographicData.csv')
+def basic_functions():
     print(len(stats))
     print(stats.columns)
     print(len(stats.columns))
@@ -12,6 +12,16 @@ def main():
     print(stats.info())
     print(stats.describe())
     print(stats.describe().transpose())
+
+
+def rename_columns():
+    print(stats.columns)
+    stats.columns = ['CountryName', 'CountryCode', 'Birthrate', 'Internetusers', 'IncomeGroup']
+    print(stats.head())
+
+
+def main():
+    rename_columns()
 
 
 if __name__ == '__main__':
