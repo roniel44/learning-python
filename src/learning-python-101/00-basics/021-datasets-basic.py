7682e90e3506e76ce75726998be59c412c2758ac
Roniel Bernas <roniel@eligeo.com>
2018-07-26 16:52:50 +0800

    learned a few functions

--- src/learning-python-101/00-basics/021-datasets-basic.py
+++ src/learning-python-101/00-basics/021-datasets-basic.py
@@ -2,7 +2,16 @@ import pandas as pd
 
 
 def main():
-    stats = pd.read_csv('somefilepath')
+    stats = pd.read_csv('files/DemographicData.csv')
+    print(len(stats))
+    print(stats.columns)
+    print(len(stats.columns))
+    print(stats.head())  # top 5
+    print(stats.head(6))  # top 6
+    print(stats.tail())
+    print(stats.info())
+    print(stats.describe())
+    print(stats.describe().transpose())
 
 
 if __name__ == '__main__':
