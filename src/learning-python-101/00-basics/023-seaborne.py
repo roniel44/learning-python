d73b352464545cd5686023985b050644fa438cb6
Roniel Bernas <roniel@eligeo.com>
2018-07-29 23:49:57 +0800

    learned about lmplots

--- src/learning-python-101/00-basics/023-seaborne.py
+++ src/learning-python-101/00-basics/023-seaborne.py
@@ -6,22 +6,33 @@ plt.rcParams['figure.figsize'] = 8, 4
 
 
 def rename_columns():
-    stats.columns = ['CountryName', 'CountryCode', 'Birthrate', 'Internetusers', 'IncomeGroup']
+    stats.columns = ['CountryName', 'CountryCode', 'Birthrate', 'InternetUsers', 'IncomeGroup']
 
 
-def seaborne_displot():
-    sns.distplot(stats['Internetusers'])
+def display_displot():
+    sns.distplot(stats['InternetUsers'])
+    sns.distplot(stats['Birthrate'])
     plt.show()
 
 
-def seaborne_boxplot():
+def display_lmplot():
+    sns.lmplot(data=stats, x='InternetUsers', y='Birthrate')
+    plt.show()
+
+
+def display_lmplot_fit_reg_false():
+    sns.lmplot(data=stats, x='InternetUsers', y='Birthrate', fit_reg=False, hue='IncomeGroup')
+    plt.show()
+
+
+def display_boxplot():
     sns.boxplot(data=stats, x='IncomeGroup', y='Birthrate')
     plt.show()
 
 
 def main():
     rename_columns()
-    seaborne_boxplot()
+    display_lmplot_fit_reg_false()
 
 
 if __name__ == '__main__':
