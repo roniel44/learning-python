36879d8144a5a72a75442162eeef6875f0677c94
Roniel Bernas <roniel@eligeo.com>
2018-07-31 23:56:25 +0800

    learned about joint plots

--- src/learning-python-101/00-basics/022-advanced-datasets.py
+++ src/learning-python-101/00-basics/022-advanced-datasets.py
@@ -1,13 +1,38 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
 movies = pd.read_csv('files/Movie-Ratings.csv')
-movies.columns = ['Film', 'Genre', 'CriticRating', 'AudienceRating', 'BudgetMillions', 'Year']
-print(movies.head())
-print(movies.info())
 
-print(movies.describe())
 
-movies.Film = movies.Film.astype('category')
-movies.Year = movies.Year.astype('category')
-print(movies.info())
+def rename_columns():
+    movies.columns = ['Film', 'Genre', 'CriticRating', 'AudienceRating', 'BudgetMillions', 'Year']
 
-print(movies.Year.cat.categories)
+
+def categorize_per_column():
+    print(movies.head())
+    print(movies.info())
+
+    print(movies.describe())
+
+    movies.Film = movies.Film.astype('category')
+    movies.Year = movies.Year.astype('category')
+    print(movies.info())
+
+    print(movies.Year.cat.categories)
+
+
+def display_joint_plot_sample():
+    sns.jointplot(data=movies, x='CriticRating', y='AudienceRating', kind='hex')
+    plt.show()
+
+
+def main():
+    rename_columns()
+    display_joint_plot_sample()
+
+
+if __name__ == '__main__':
+    main()
